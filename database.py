3639c54882fec631f84aece596d8198730d5ff6c
daniel-david1 <daniel@daniel-social.com>
2026-03-22 15:30:31 +0200

    Update database.py

--- database.py
+++ database.py
@@ -7,7 +7,7 @@ logger = logging.getLogger(__name__)
 DB_PATH = "/tmp/secretary.db"
 
 def get_conn():
-    os.makedirs("/data", exist_ok=True)
+    os.makedirs("/tmp", exist_ok=True)
     conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
     return conn
